f0633f3ca7be856e476926a95f4a10d2f347726d
hgcummings <harry@xlr3.com>
2020-10-03 18:17:08 +0100

    Tweak speeds

--- src/models/game.ts
+++ src/models/game.ts
@@ -7,11 +7,11 @@ export default class implements Model {
     debug: boolean = false;
     grid = [
         { tiles: 1, type: 'land', speed: -0.1 * Math.PI },
-        { tiles: 6, type: 'pond', speed: -0.2 * Math.PI },
-        { tiles: 12, type: 'pond', speed: -0.3 * Math.PI },
-        { tiles: 24, type: 'pond', speed: 0.4 * Math.PI },
-        { tiles: 24, type: 'pond', speed: 0.5 * Math.PI },
-        { tiles: 24, type: 'pond', speed: 0.6 * Math.PI },
+        { tiles: 6, type: 'pond', speed: -0.15 * Math.PI },
+        { tiles: 12, type: 'pond', speed: -0.2 * Math.PI },
+        { tiles: 24, type: 'pond', speed: 0.25 * Math.PI },
+        { tiles: 24, type: 'pond', speed: 0.3 * Math.PI },
+        { tiles: 24, type: 'pond', speed: 0.35 * Math.PI },
         { tiles: 48, type: 'land' },
         { tiles: 48, type: 'road' },
         { tiles: 48, type: 'road' },
